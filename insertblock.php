87749e9f2bf8f8313d2f8803af56a24b9b1e91da
Saswat1999 <47583914+Saswat1999@users.noreply.github.com>
2019-03-17 16:03:23 +0530

    Create insertblock.php

---
+++ insertblock.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test2";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+if($flag == 1)
+{
+$sql = "INSERT INTO block (place, point, flag)
+VALUES ('patia sector-6', '$point', '$flag')";
+}
+include 'update.php';
+
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
