cb5a1e6082c85aeee3b1906a892814178c9bf753
Saswat1999 <47583914+Saswat1999@users.noreply.github.com>
2019-03-17 15:59:52 +0530

    Rename createblock.php to createblock1.php

---
+++ createblock1.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test2";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql to create table
+$sql = "CREATE TABLE sub (
+id INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+place VARCHAR(30) NOT NULL,
+point FLOAT(5) NOT NULL,
+flag INT(1) NOT NULL,
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table data created sucessfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
+
